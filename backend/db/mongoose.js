e8c4fc7d9c406696b857612de0f20f84a8370693
AvinavTyagi <avinav.tyagi@sjsu.edu>
2020-10-06 14:09:41 -0700

    db changes

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  "mongodb+srv://nachiket:nachiket@cmpe280-emqpv.mongodb.net/test?retryWrites=true&w=majority",{
+  
     poolSize: 10
     // other options can go here
   },
